02feea924fe36c9a45bc8a7f7fb7b56ec537c2db
deepthikada <kdeepthi_cse2305f6@mgit.ac.in>
2025-12-25 12:30:27 +0530

    Phase 1: Initial project setup for AutoBias+

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="AutoBias+", layout="wide")
+
+st.title("AutoBias+")
+st.subheader("Automated Bias Detection & Mitigation Tool for ML Datasets")
+
+st.write("Project setup successful 🚀")
